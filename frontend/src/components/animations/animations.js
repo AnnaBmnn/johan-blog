6e5762647185a5287446ae669c280fc41f798e80
Anna <annabaumann@laposte.net>
2020-11-12 16:44:22 +0100

    animation vase + size burger menu

--- frontend/src/components/animations/animations.js
+++ frontend/src/components/animations/animations.js
@@ -5,15 +5,17 @@ import animationFire from "../../json/fire.json"
 import animationFireTache from "../../images/fire__tache.png"
 import animationObjects from "../../json/objects.json"
 import animationLeef from "../../json/leef.json"
+import animationVase from "../../json/vase.json"
 import imgGuitare from "../../images/guitare.svg"
 import imgTente from "../../images/tente.svg"
-import imgVase from "../../images/vase.svg"
+// import imgVase from "../../images/vase.svg"
 import animationsStyles from "./animations.module.scss"
 
 const App = () => {
   let animationFireContainer = createRef()
   let animationObjectsContainer = createRef()
   let animationLeefContainer = createRef()
+  let animationVaseContainer = createRef()
 
   useEffect(() => {
     const animFire = lottie.loadAnimation({
@@ -37,10 +39,18 @@ const App = () => {
       autoplay: true,
       animationData: animationLeef,
     })
+    const animVase = lottie.loadAnimation({
+      container: animationVaseContainer.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: animationVase,
+    })
     return () => {
       animFire.destroy()
       animLeef.destroy()
       animObjects.destroy()
+      animVase.destroy()
     }
   }, [])
 
@@ -68,11 +78,9 @@ const App = () => {
         src={imgGuitare}
         alt="la guitare"
       />
-
-      <img
+      <div
         className={`${animationsStyles.animations__vase}`}
-        src={imgVase}
-        alt="le vase"
+        ref={animationVaseContainer}
       />
       <img
         className={`${animationsStyles.animations__tente}`}
